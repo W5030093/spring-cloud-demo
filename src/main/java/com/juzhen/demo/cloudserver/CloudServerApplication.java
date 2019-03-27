769e295961460e2a8fa247527f7a3ab143b6f70c
W5030093 <wyc11031@163.com>
2019-03-27 22:34:45 +0800

    Initial commit

---
+++ src/main/java/com/juzhen/demo/cloudserver/CloudServerApplication.java
@@ -0,0 +1,13 @@
+package com.juzhen.demo.cloudserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudServerApplication.class, args);
+    }
+
+}
