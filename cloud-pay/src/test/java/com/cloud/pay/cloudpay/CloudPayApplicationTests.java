aadbab5c076e3ee407c0da1e99ef89113217d01f
W5030093 <wyc11031@163.com>
2019-07-14 17:31:24 +0800

    12

--- cloud-pay/src/test/java/com/cloud/pay/cloudpay/CloudPayApplicationTests.java
+++ cloud-pay/src/test/java/com/cloud/pay/cloudpay/CloudPayApplicationTests.java
@@ -1,16 +1,31 @@
 package com.cloud.pay.cloudpay;
 
+import me.chanjar.weixin.common.error.WxErrorException;
+import me.chanjar.weixin.mp.api.WxMpService;
+import me.chanjar.weixin.mp.api.WxMpTemplateMsgService;
+import me.chanjar.weixin.mp.bean.template.WxMpTemplate;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.inject.Inject;
+import java.util.List;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CloudPayApplicationTests {
 
+    @Inject
+    protected WxMpService wxService;
+
+
+
     @Test
-    public void contextLoads() {
+    public void contextLoads() throws WxErrorException {
+        List<WxMpTemplate> allPrivateTemplate =
+                wxService.getTemplateMsgService().getAllPrivateTemplate();
+        System.out.println(allPrivateTemplate.toString());
     }
 
 }
